c3cc291ce591fe018c96ac33141917df06d434c5
Michael Urisman <michael.urisman@gmail.com>
2023-07-26 18:43:52 +0300

    Main.java is updated according to the task

--- src/main/java/cc/robotdreams/lesson16/Main.java
+++ src/main/java/cc/robotdreams/lesson16/Main.java
@@ -1,19 +1,39 @@
 package cc.robotdreams.lesson16;
 
+import cc.robotdreams.lesson16.exceptions.WrongAccountException;
+import cc.robotdreams.lesson16.exceptions.WrongCurrencyException;
+import cc.robotdreams.lesson16.exceptions.WrongOperationException;
+
 public class Main
 {
-    // Поле BankApplication
+    static BankApplication bankApplication = new BankApplication();
+    static int counter = 1;
 
-    static public void main(String[] args){
-        /*
-            Викликати метод processWrapper з наступними параметрами:
-            id = accountId000, amount 50, currency USD
-            id = accountId003, amount 250, currency HRV
-            id = accountId001, amount 50, currency EUR
-            id = accountId001, amount 50, currency USD
-            id = accountId001, amount 50, currency USD
-         */
+    static public void main(String[] args) {
+
+        processWrapper ("accountId000", 50, "USD");     //wrong account
+        processWrapper ("accountId003", 250, "HRV");    //boundary positive value + random exception
+        processWrapper ("accountId001", 50, "EUR");     //wrong currency
+        processWrapper ("accountId001", 50, "USD");     //positive value + random exception
+        processWrapper ("accountId001", 2000, "USD");   //wrong amount
     }
 
-    // Метод processWrapper(String, int, String)
+    static public void processWrapper (String id, int amount, String currency) {
+        System.out.printf("Case %s: %s-%s-%s\n", counter++, id, amount, currency);
+
+        try {
+            bankApplication.process(id, amount, currency);
+        } catch (WrongAccountException e) {
+            System.out.println("Такого акаунту не існує");
+        } catch (WrongCurrencyException e) {
+            System.out.println("Акаунт має рахунок в іншій валюті");
+        } catch (WrongOperationException e) {
+            System.out.println("Акаунт не має достатньо коштів");
+        } catch (Exception e) {
+            System.out.println("Cталася помилка при процесінгу, спробуйте ще раз");
+        } finally {
+            System.out.println("Дякуємо, що скористалися нашим сервісом");
+            System.out.println("---------------------------------------");
+        }
+    }
 }
