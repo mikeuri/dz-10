546bac2d73e496fa1901540d48770298a111d5d5
Michael Urisman <michael.urisman@gmail.com>
2023-07-26 18:36:55 +0300

    initial project

---
+++ src/main/java/cc/robotdreams/lesson16/Main.java
@@ -0,0 +1,19 @@
+package cc.robotdreams.lesson16;
+
+public class Main
+{
+    // Поле BankApplication
+
+    static public void main(String[] args){
+        /*
+            Викликати метод processWrapper з наступними параметрами:
+            id = accountId000, amount 50, currency USD
+            id = accountId003, amount 250, currency HRV
+            id = accountId001, amount 50, currency EUR
+            id = accountId001, amount 50, currency USD
+            id = accountId001, amount 50, currency USD
+         */
+    }
+
+    // Метод processWrapper(String, int, String)
+}
